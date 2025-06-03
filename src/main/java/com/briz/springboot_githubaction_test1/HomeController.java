5c7ea67f9d9eac818eb7946acafa06afe35000b8
kanchan raju <kanchanraju2015@gmail.com>
2025-06-03 16:14:08 +0530

    git action commit

--- src/main/java/com/briz/springboot_githubaction_test1/HomeController.java
+++ src/main/java/com/briz/springboot_githubaction_test1/HomeController.java
@@ -3,6 +3,13 @@ package com.briz.springboot_githubaction_test1;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+// first create the springboot project then push to the github first 
+// now go to actions and see the new workflow with maven 
+// yml file must be unique 
+//change some code into bottom 
+
+
 @RestController
 public class HomeController
 {
@@ -10,5 +17,10 @@ public class HomeController
 public String test()
 {
 	return "this is jenkins test";
+}
+@RequestMapping("/git")
+public String git()
+{
+	return "this is git action";
 }
 }
--- src/main/java/com/briz/springboot_githubaction_test1/SpringbootGithubactionTest1Application.java
+++ src/main/java/com/briz/springboot_githubaction_test1/SpringbootGithubactionTest1Application.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootGithubactionTest1Application {
+public class SpringbootGithubactionTest1Application
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootGithubactionTest1Application.class, args);
